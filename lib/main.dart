d21651756c160c642928960103176c905046ac13
Aura11 <81680756+Aura11@users.noreply.github.com>
2023-02-12 18:43:50 +0300

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          backgroundColor: Colors.blue,
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+int buttonPressedd = 1;
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            setState(() {
+              buttonPressedd = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image(image: AssetImage('images/ball$buttonPressedd.png'))),
+    );
+  }
+}
